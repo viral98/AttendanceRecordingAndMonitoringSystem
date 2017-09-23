bc092539e428fc543c7852ee0ddc2d9f1b3bccd2
Viral Tagdiwala <tagdiwalaviral@gmail.com>
2017-09-23 21:57:07 +0530

    Directory Structure

--- time.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-date_default_timezone_set("aisa/kolkata");
-echo "The time is " . time();*/
-date_default_timezone_set('Asia/Kolkata');
-echo date('h-i-s');
-$x=getdate();
-echo $x[weekday];
-?>
